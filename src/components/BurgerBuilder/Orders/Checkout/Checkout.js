dc7026a279ee4370517f99c59d57c35b96e0e17b
Saiful-Lab <saifulislam_@outlook.com>
2022-08-21 13:14:52 +0600

    add firebase database

--- src/components/BurgerBuilder/Orders/Checkout/Checkout.js
+++ src/components/BurgerBuilder/Orders/Checkout/Checkout.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { Button } from 'reactstrap'
+import { Button, Moda } from 'reactstrap'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import Spinner from '../../../Spinner/Spinner'
 
 const mapStateToProps = state => ({
   ingredients: state.ingredients,
@@ -13,7 +14,8 @@ class Checkout extends Component {
   state = {
     delivaryAddress: '',
     phone: '',
-    paymentType: 'Cash On Delivaray'
+    paymentType: 'Cash On Delivaray',
+    isLoading: false
   }
 
   handleInputChange = e => {
@@ -25,63 +27,88 @@ class Checkout extends Component {
 
   handleSubmit = e => {
     e.preventDefault()
-    console.log(this.state);
+    this.setState({
+      isLoading: true
+    })
+    const order = {
+      ingredients: this.props.ingredients,
+      price: this.props.totalPrice,
+      customer: this.state,
+      orderTime: new Date()
+    }
+    // console.log(order);
+    fetch('https://burger-builder-55d2b-default-rtdb.firebaseio.com/orders.json', {
+      method: 'POST',
+      body: JSON.stringify(order)
+    }).then(
+      () => this.setState({
+        isLoading: false
+      })
+    ).catch(
+      () => this.setState({
+        isLoading: false
+      })
+    )
   }
 
   render() {
+    const form = <div>
+      <h4 style={{
+        border: "1px solid gray",
+        boxShadow: '1px 1px #888',
+        borderRadius: '5px',
+        padding: '20px'
+      }}
+      >
+        Payment: {this.props.totalPrice} BDT
+      </h4>
+      <form style={{
+        border: "1px solid gray",
+        boxShadow: '1px 1px #888',
+        borderRadius: '5px',
+        padding: '20px'
+      }}>
+        <input
+          type="textarea"
+          className='form-control'
+          placeholder='your address'
+          name='delivaryAddress'
+          value={this.delivaryAddress}
+          onChange={(e) => this.handleInputChange(e)}
+        />
+        <br />
+        <input
+          type="text"
+          className='form-control'
+          placeholder='your number'
+          name='phone'
+          value={this.phone}
+          onChange={(e) => this.handleInputChange(e)}
+        />
+        <br />
+        <select
+          name='paymentType'
+          value={this.paymentType}
+          className='form-control'
+          onChange={(e) => this.handleInputChange(e)}
+        >
+          <option value="Cash On Delivary">Cash On Delevary</option>
+          <option value="bKas">bKash</option>
+        </select>
+        <br />
+        <Button style={{ backgroundColor: "#d70f64" }}
+          onClick={this.handleSubmit}
+          disabled={!this.props.purchassble}
+        >Place Order</Button>
+        <Link to="/">
+          <Button color='secondary'
+            onClick={this.goBack} className="ml-3">Cancle</Button>
+        </Link>
+      </form>
+    </div>
     return (
       <div>
-        <h4 style={{
-          border: "1px solid gray",
-          boxShadow: '1px 1px #888',
-          borderRadius: '5px',
-          padding: '20px'
-        }}
-        >
-          Payment: {this.props.totalPrice} BDT
-        </h4>
-        <form style={{
-          border: "1px solid gray",
-          boxShadow: '1px 1px #888',
-          borderRadius: '5px',
-          padding: '20px'
-        }}>
-          <input
-            type="textarea"
-            className='form-control'
-            placeholder='your address'
-            name='delivaryAddress'
-            value={this.delivaryAddress}
-            onChange={(e) => this.handleInputChange(e)}
-          />
-          <br />
-          <input
-            type="text"
-            className='form-control'
-            placeholder='your number'
-            name='phone'
-            value={this.phone}
-            onChange={(e) => this.handleInputChange(e)}
-          />
-          <br />
-          <select
-            name='paymentType'
-            value={this.paymentType}
-            className='form-control'
-            onChange={(e) => this.handleInputChange(e)}
-          >
-            <option value="Cash On Delivary">Cash On Delevary</option>
-            <option value="bKas">bKash</option>
-          </select>
-          <br />
-          <Button style={{ backgroundColor: "#d70f64" }}
-            onClick={this.handleSubmit}
-          >Place Order</Button>
-          <Link to="/">
-            <Button color='secondary'
-              onClick={this.goBack} className="ml-3">Cancle</Button>
-          </Link>
-        </form>
+        {this.state.isLoading ? <Spinner /> : form }
       </div>
     )
   }
---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Spinner.css'
+
+function Spinner() {
+  return (
+      <div className="loader">Loading...</div>
+  )
+}
+
+export default Spinner
